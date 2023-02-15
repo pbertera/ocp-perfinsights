696023cbff0cb5ffe37129b45099242a8579056d
Pietro Bertera <pietro@bertera.it>
2023-02-15 16:21:14 +0100

    ADD: volumes

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ IMAGE=quay.io/pbertera/ocp-perfinsights
 
 sudo podman run --hostname $NAME \
 	--name $NAME \
+	--rm \
 	--privileged \
 	--net=host \
 	--pid=host \
@@ -12,8 +13,10 @@ sudo podman run --hostname $NAME \
 	--tty \
 	--interactive \
 	--security-opt label=disable \
+	--volume /sys:/sys \
 	--volume /run:/run \
 	--volume /var/log:/var/log \
 	--volume /etc/machine-id:/etc/machine-id \
 	--volume /etc/localtime:/etc/localtime \
+	--volume /etc/redhat-release:/etc/redhat-release \
 	$IMAGE $@
